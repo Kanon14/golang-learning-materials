94ee91f182ca1183db0e0e7814310f7511edd719
Kanon14 <geochua144@gmail.com>
2026-02-18 21:35:02 +0800

    added fmt info to chapter 6

---
+++ chp06_console_output/demo.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := false
+	fmt.Println("hello", 2, x) // Print line
+	fmt.Printf("%v", x)        // Print format string; %T (Type); %v (default value)
+}
+
+// fmt package overview: https://pkg.go.dev/fmt
